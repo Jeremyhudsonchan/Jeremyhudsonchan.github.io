50e910ec48be0a303add55272e0c80293dc910b0
Jeremyhudsonchan <jeremyhudsonchan@gmail.com>
2023-10-21 02:27:51 -0400

    Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,26 +134,37 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+const navbarLinks = document.querySelectorAll('[data-nav-link]');
 
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    const sectionId = link.dataset.navLink;
+    const section = document.getElementById(sectionId);
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
-
   });
-}
+});
+
+// // page navigation variables
+// const navigationLinks = document.querySelectorAll("[data-nav-link]");
+// const pages = document.querySelectorAll("[data-page]");
+
+// // add event to all nav link
+// for (let i = 0; i < navigationLinks.length; i++) {
+//   navigationLinks[i].addEventListener("click", function () {
+
+//     for (let i = 0; i < pages.length; i++) {
+//       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+//         pages[i].classList.add("active");
+//         navigationLinks[i].classList.add("active");
+//         window.scrollTo(0, 0);
+//       } else {
+//         pages[i].classList.remove("active");
+//         navigationLinks[i].classList.remove("active");
+//       }
+//     }
+
+//   });
+// }
